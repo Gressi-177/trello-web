5d2e0de7198a10726c9bfc6b5bccd4d6202ecc23
VietDoan <vietdoan177@gmail.com>
2024-06-21 13:44:59 +0700

    Install MUI and husky

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Button } from '@mui/material'
+import SvgIcon from '@mui/material/SvgIcon'
+import { pink } from '@mui/material/colors'
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d='M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z' />
+    </SvgIcon>
+  )
+}
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Button variant='text'>Text</Button>
+      <Button variant='contained'>Contained</Button>
+      <Button variant='outlined'>Outlined</Button>
+
+      <br />
+      <HomeIcon />
+      <HomeIcon color='primary' />
+      <HomeIcon color='secondary' />
+      <HomeIcon color='success' />
+      <HomeIcon color='action' />
+      <HomeIcon color='disabled' />
+      <HomeIcon sx={{ color: pink[500] }} />
     </>
   )
 }
