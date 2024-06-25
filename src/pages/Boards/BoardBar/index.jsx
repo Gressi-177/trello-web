b19ba70a6b2bd4b72d7a07e7ce73cf68e6569c7c
VietDoan <vietdoan177@gmail.com>
2024-06-25 16:13:38 +0700

    chore: Update avatar size in Profile component

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,16 +1,168 @@
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import Avatar from '@mui/material/Avatar'
+import AvatarGroup from '@mui/material/AvatarGroup'
 import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Chip from '@mui/material/Chip'
+import Tooltip from '@mui/material/Tooltip'
 function BoardBar() {
   return (
     <Box
       sx={{
-        backgroundColor: 'primary.dark',
         width: '100%',
         height: (theme) => theme.trello.boardBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        paddingX: 2,
+        overflowX: 'auto',
+        borderTop: '1px solid #e0e0e0'
       }}
     >
-      Board bar
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          sx={{
+            color: 'primary.main',
+            bgcolor: 'white',
+            border: 'none',
+            paddingX: '5px',
+            borderRadius: '4px',
+            '& .MuiSvgIcon-root': {
+              color: 'primary.main'
+            },
+            '&:hover': {
+              bgColor: 'primary.50'
+            }
+          }}
+          icon={<DashboardIcon />}
+          label='Viet Doan'
+          clickable
+        />
+        <Chip
+          sx={{
+            color: 'primary.main',
+            bgcolor: 'white',
+            border: 'none',
+            paddingX: '5px',
+            borderRadius: '4px',
+            '& .MuiSvgIcon-root': {
+              color: 'primary.main'
+            },
+            '&:hover': {
+              bgColor: 'primary.50'
+            }
+          }}
+          icon={<VpnLockIcon />}
+          label='Public / Private Workspace'
+          clickable
+        />
+        <Chip
+          sx={{
+            color: 'primary.main',
+            bgcolor: 'white',
+            border: 'none',
+            paddingX: '5px',
+            borderRadius: '4px',
+            '& .MuiSvgIcon-root': {
+              color: 'primary.main'
+            },
+            '&:hover': {
+              bgColor: 'primary.50'
+            }
+          }}
+          icon={<AddToDriveIcon />}
+          label='Add to Google Drive'
+          clickable
+        />
+        <Chip
+          sx={{
+            color: 'primary.main',
+            bgcolor: 'white',
+            border: 'none',
+            paddingX: '5px',
+            borderRadius: '4px',
+            '& .MuiSvgIcon-root': {
+              color: 'primary.main'
+            },
+            '&:hover': {
+              bgColor: 'primary.50'
+            }
+          }}
+          icon={<BoltIcon />}
+          label='Automation'
+          clickable
+        />
+        <Chip
+          sx={{
+            color: 'primary.main',
+            bgcolor: 'white',
+            border: 'none',
+            paddingX: '5px',
+            borderRadius: '4px',
+            '& .MuiSvgIcon-root': {
+              color: 'primary.main'
+            },
+            '&:hover': {
+              bgColor: 'primary.50'
+            }
+          }}
+          icon={<FilterListIcon />}
+          label='Filters'
+          clickable
+        />
+      </Box>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Button variant='outlined' startIcon={<PersonAddIcon />}>
+          Delete
+        </Button>
+        <AvatarGroup
+          max={4}
+          sx={{
+            '& .MuiAvatar-root': {
+              width: 34,
+              height: 34,
+              fontSize: 16
+            }
+          }}
+        >
+          <Tooltip title='vietdoan'>
+            <Avatar
+              alt='vietdoan'
+              src='https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-1/442410842_1547368989153636_6278656583367096292_n.jpg?stp=dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=YaaIx-L1IYIQ7kNvgHpQF6v&_nc_ht=scontent-sin6-2.xx&oh=00_AYAwC9Xr-2VwdEILQ06ptm9S4Y7EDd7E4xBiQlj5G3ZZnQ&oe=667F0BD9'
+            />
+          </Tooltip>
+          <Tooltip title='vietdoan'>
+            <Avatar
+              alt='vietdoan'
+              src='https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-1/442410842_1547368989153636_6278656583367096292_n.jpg?stp=dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=YaaIx-L1IYIQ7kNvgHpQF6v&_nc_ht=scontent-sin6-2.xx&oh=00_AYAwC9Xr-2VwdEILQ06ptm9S4Y7EDd7E4xBiQlj5G3ZZnQ&oe=667F0BD9'
+            />
+          </Tooltip>
+          <Tooltip title='vietdoan'>
+            <Avatar
+              alt='vietdoan'
+              src='https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-1/442410842_1547368989153636_6278656583367096292_n.jpg?stp=dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=YaaIx-L1IYIQ7kNvgHpQF6v&_nc_ht=scontent-sin6-2.xx&oh=00_AYAwC9Xr-2VwdEILQ06ptm9S4Y7EDd7E4xBiQlj5G3ZZnQ&oe=667F0BD9'
+            />
+          </Tooltip>
+          <Tooltip title='vietdoan'>
+            <Avatar
+              alt='vietdoan'
+              src='https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-1/442410842_1547368989153636_6278656583367096292_n.jpg?stp=dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=YaaIx-L1IYIQ7kNvgHpQF6v&_nc_ht=scontent-sin6-2.xx&oh=00_AYAwC9Xr-2VwdEILQ06ptm9S4Y7EDd7E4xBiQlj5G3ZZnQ&oe=667F0BD9'
+            />
+          </Tooltip>
+          <Tooltip title='vietdoan'>
+            <Avatar
+              alt='vietdoan'
+              src='https://scontent-sin6-2.xx.fbcdn.net/v/t39.30808-1/442410842_1547368989153636_6278656583367096292_n.jpg?stp=dst-jpg_p200x200&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=YaaIx-L1IYIQ7kNvgHpQF6v&_nc_ht=scontent-sin6-2.xx&oh=00_AYAwC9Xr-2VwdEILQ06ptm9S4Y7EDd7E4xBiQlj5G3ZZnQ&oe=667F0BD9'
+            />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
